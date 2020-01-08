13fc38a557716edd4615b4b89bf0b264c3fee441
TobiOladoja <tbfullstack@gmail.com>
2020-01-08 22:47:54 +0000

    Got the charts working, create stats and added data, aswell as rendering the display

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,7 +2,31 @@ import React, { Component } from 'react';
 import { Bar, Line, Pie } from 'react-chartjs-2';
 
 export default class Chart extends Component {
+  constructor(props) {
+    super(props); // Props are properties you can pass into components
+    this.state = {
+      // To create the state
+      chartData: {
+        labels: ['London', 'Manchester', 'Leeds', 'Birmingham', 'Coventry'],
+        datasets: [
+          {
+            label: 'Population',
+            data: [10000, 8000, 6000, 5500, 2300],
+            backgroundColor: ['blue', 'red', 'green', 'yellow', 'orange']
+          }
+        ]
+      }
+    };
+  }
+
   render() {
-    return <div className='chart'>CHART COMPONENT</div>;
+    return (
+      <div className='chart'>
+        <Bar
+          data={this.state.chartData}
+          options={{ maintainAspectRatio: false }}
+        />
+      </div>
+    );
   }
 }
